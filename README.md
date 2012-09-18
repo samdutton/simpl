cc08174f14b34c1f6073698639bcd811394917d9
samdutton <samdutton@gmail.com>
2012-09-18 21:35:16 +0100

    fixed README link

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 simpl
 =====
 
-[simpl.info](simpl.info) is a website for simplest-possible code examples, nothing more. 
+[simpl.info](http://www.simpl.info) is a website for simplest-possible code examples, nothing more. 
 
 I also maintain a [list of resources](https://docs.google.com/document/d/1kuhUEiIkAUHO0LBBNM4A1Enc5QgVZn3CDkuSoPZJR1U/edit#) for information about HTML, CSS and JavaScript. 
 
--- indexeddb/js/main.js
+++ indexeddb/js/main.js
@@ -1,4 +1,46 @@
 window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
+
+// This is what our customer data looks like.
+const customerData = [
+  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
+];
+const dbName = "the_name";
+
+var request = window.indexedDB.open(dbName, 2);
+
+request.onerror = function(event) {
+  // Handle errors.
+};
+request.onsuccess = function(event) {
+  console.log("success", event);
+};
+request.onupgradeneeded = function(event) {
+	console.log("event: ", event);
+  // Create an objectStore to hold information about our customers. We're
+  // going to use "ssn" as our key path because it's guaranteed to be
+  // unique.
+  var objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
+
+  // Create an index to search customers by name. We may have duplicates
+  // so we can't use a unique index.
+  objectStore.createIndex("name", "name", { unique: false });
+
+  // Create an index to search customers by email. We want to ensure that
+  // no two customers have the same email, so use a unique index.
+  objectStore.createIndex("email", "email", { unique: true });
+
+  // Store values in the newly created objectStore.
+  for (i in customerData) {
+    objectStore.add(customerData[i]);
+  }
+};
+
+
+
+/*
+
+window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
 var request = window.indexedDB.open("CandyDB");
 request.onsuccess = function(event) {
 console.log(event);
@@ -32,6 +74,8 @@ console.log(event);
     loadData(db);
   }
 };
+
+*/
 
 /*
 
