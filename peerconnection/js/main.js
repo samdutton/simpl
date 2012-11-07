797ff062191d8830ad16e8e5034a40dea73cde54
samdutton <samdutton@gmail.com>
2012-11-07 15:51:31 +0000

    fixed track

--- peerconnection/js/main.js
+++ peerconnection/js/main.js
@@ -1,3 +1,5 @@
+performance.now = performance.now || performance.webkitNow; // hack added by SD!
+
 //var vid1 = document.getElementById("vid1");
 //var vid2 = document.getElementById("vid2");
 btn1.disabled = false;
@@ -11,7 +13,7 @@ function trace(text) {
   if (text[text.length - 1] == '\n') {
     text = text.substring(0, text.length - 1);
   }
-  console.log((performance.webkitNow() / 1000).toFixed(3) + ": " + text);
+  console.log((performance.now() / 1000).toFixed(3) + ": " + text);
 }
 
 function gotStream(stream){
@@ -26,48 +28,47 @@ function start() {
   btn1.disabled = true;
   navigator.webkitGetUserMedia({audio:true, video:true},
                                gotStream, function() {});
-}  
-  
+}
+
 function call() {
   btn2.disabled = true;
   btn3.disabled = false;
   trace("Starting call");
   if (localstream.videoTracks.length > 0)
-    trace('Using Video device: ' + localstream.videoTracks[0].label);  
+    trace('Using Video device: ' + localstream.videoTracks[0].label);
   if (localstream.audioTracks.length > 0)
     trace('Using Audio device: ' + localstream.audioTracks[0].label);
-
-  pc1 = new webkitPeerConnection00(null, iceCallback1);
-  trace("Created local peer connection object pc1"); 
-  pc2 = new webkitPeerConnection00(null, iceCallback2);
+  var servers = null;
+  pc1 = new webkitRTCPeerConnection(servers);
+  trace("Created local peer connection object pc1");
+  pc1.onicecandidate = iceCallback1;
+  pc2 = new webkitRTCPeerConnection(servers);
   trace("Created remote peer connection object pc2");
-  pc2.onaddstream = gotRemoteStream; 
+  pc2.onicecandidate = iceCallback2;
+  pc2.onaddstream = gotRemoteStream;
 
   pc1.addStream(localstream);
   trace("Adding Local Stream to peer connection");
-  var offer = pc1.createOffer(null);
-  trace("Created offer:\n" + offer.toSdp());
-  pc1.setLocalDescription(pc1.SDP_OFFER, offer);
-  trace("SetLocalDesc1");
-  pc2.setRemoteDescription(pc2.SDP_OFFER, offer);
-  trace("SetRemoteDesc2");
-  //ta1.value = offer.toSdp();
-  var answer = pc2.createAnswer(offer.toSdp(),
-                                {has_audio:true, has_video:true});
-  trace("Created answer:\n" + answer.toSdp());
-  pc2.setLocalDescription(pc2.SDP_ANSWER, answer);
-  trace("SetLocalDesc2");
-  pc1.setRemoteDescription(pc1.SDP_ANSWER, answer);
-  trace("SetRemoteDesc1");
-  //ta2.value = answer.toSdp();
-  pc1.startIce();
-  pc2.startIce();
-  trace("Started ICE for both local & remote");
+
+  pc1.createOffer(gotDescription1);
+}
+
+function gotDescription1(desc){
+  pc1.setLocalDescription(desc);
+  trace("Offer from pc1 \n" + desc.sdp);
+  pc2.setRemoteDescription(desc);
+  pc2.createAnswer(gotDescription2);
+}
+
+function gotDescription2(desc){
+  pc2.setLocalDescription(desc);
+  trace("Answer from pc2 \n" + desc.sdp);
+  pc1.setRemoteDescription(desc);
 }
 
 function hangup() {
   trace("Ending call");
-  pc1.close(); 
+  pc1.close();
   pc2.close();
   pc1 = null;
   pc2 = null;
@@ -80,16 +81,17 @@ function gotRemoteStream(e){
   trace("Received remote stream");
 }
 
-function iceCallback1(candidate,bMore){
-  if (candidate) {
-    pc2.processIceMessage(candidate);
-    trace("Local ICE candidate: " + candidate.toSdp());
+function iceCallback1(event){
+  if (event.candidate) {
+    pc2.addIceCandidate(new RTCIceCandidate(event.candidate));
+    trace("Local ICE candidate: \n" + event.candidate.candidate);
   }
 }
-      
-function iceCallback2(candidate,bMore){
-  if (candidate) {
-    pc1.processIceMessage(candidate);
-    trace("Remote ICE candidate: " + candidate.toSdp());
+
+function iceCallback2(event){
+  if (event.candidate) {
+    pc1.addIceCandidate(new RTCIceCandidate(event.candidate));
+    trace("Remote ICE candidate: \n " + event.candidate.candidate);
   }
 }
+
