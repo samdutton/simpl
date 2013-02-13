3895d41d998f7668da3fe245d0460815bb283b44
samdutton <samdutton@gmail.com>
2013-02-13 14:12:07 +0000

    Commit before removing meta element for encoding

--- idbwrapper/js/main.js
+++ idbwrapper/js/main.js
@@ -1,57 +1,28 @@
-var dataElement = document.getElementById("data");
-var findButton = document.getElementById("findButton");
-var storeButton = document.getElementById("storeButton");
-var artist = document.getElementById("artist");
-var song = document.getElementById("song");
-var query = document.getElementById("query");
-
-var songs = new IDBStore({
+var customers = new IDBStore({
   dbVersion: 1,
-  storeName: 'songs',
+  storeName: 'customer',
   keyPath: 'id',
   autoIncrement: true,
-  onStoreReady: handleStoreReady,
-  indexes: [
-    {name: "artist"/*, keyPath: 'songName', unique: false, multiEntry: false */},
-    {name: "song"}
+  onStoreReady: function(){
+    console.log('Store ready!');
+  }
+});
+
+var dude = {
+  firstname: 'John',
+  lastname: 'Doe',
+  age: 52,
+  emails: [
+    'johndoe@example.com',
+    'jd@example.com'
   ]
-});
+};
 
-console.log(songs);
-
-function addSong(artist, song){
-  console.log("Add song: ", artist, song);
-  var song = {
-    "artist": artist,
-    "song": song
-  };
-  songs.put(song, handleSuccess, handleError);
+var onsuccess = function(id){
+  console.log('Yeah, dude inserted! insertId is: ' + id);
+}
+var onerror = function(error){
+  console.log('Oh noes, sth went wrong!', error);
 }
 
-function findSong(text) {
-}
-
-function handleStoreReady() {
-  log("Store ready!");
-}
-
-function handleSuccess(id) {
-  log("Song added, id: " + id);
-}
-
-function handleError(error) {
-  log("Sorry. Something went wrong: " + error);
-}
-
-function log(message){
-  dataElement.innerHTML = message + "<br /><br />" + dataElement.innerHTML;
-}
-
-storeButton.addEventListener("click", function(){
-  addSong(artist.value, song.value);
-});
-
-findButton.addEventListener("click", function(){
-  findSong(query.value);
-});
-
+customers.put(dude, onsuccess, onerror);
