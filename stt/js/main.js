5a8ac3f58fee7c31b824e052c34ca184640aece1
Sam Dutton <samdutton@gmail.com>
2013-08-14 18:39:33 +0100

    Added text to speech

--- stt/js/main.js
+++ stt/js/main.js
@@ -3,13 +3,42 @@ function log(message){
   data.innerHTML += message + "<br />";
 }
 
-var u = new SpeechSynthesisUtterance();
-u.text = 'Hello world';
-u.lang = 'en-US';
-u.rate = 1.2;
-u.onend = function(event) {
-  log('Finished in ' + event.elapsedTime + ' seconds.');
-}
-speechSynthesis.speak(u);
 
-speechSynthesis.speak(new SpeechSynthesisUtterance('Hello another world'));
+var recognizing;
+var recognition = new SpeechRecognition();
+recognition.continuous = true;
+recognition.interim = true;
+reset();
+recognition.onend = reset;
+
+recognition.onresult = function (event) {
+  var final = "";
+  var interim = "";
+  for (var i = 0; i < event.results.length; ++i) {
+    if (event.results[i].final) {
+      final += event.results[i][0].transcript;
+    } else {
+      interim += event.results[i][0].transcript;
+    }
+  }
+  final_span.innerHTML = final;
+  interim_span.innerHTML = interim;
+}
+
+function reset() {
+  recognizing = false;
+  button.innerHTML = "Click to Speak";
+}
+
+function toggleStartStop() {
+  if (recognizing) {
+    recognition.stop();
+    reset();
+  } else {
+    recognition.start();
+    recognizing = true;
+    button.innerHTML = "Click to Stop";
+    final_span.innerHTML = "";
+    interim_span.innerHTML = "";
+  }
+}
