e2208e98a7978600d33ec91ff80814b51615130a
Léo-Paul COUTURIER <leopaul3@gmail.com>
2014-11-03 11:26:18 +0100

    Update the battery example to match the spec
    Uses the `navigator.getBattery()` promise when available instead of the deprecated `navigator.battery`

--- battery/js/main.js
+++ battery/js/main.js
@@ -4,13 +4,21 @@ function log(message){
   document.querySelector("#data").innerHTML += message + "<br />";
 }
 
-if (battery) {
+function logBattery(battery) {
 	log("Battery level: " + battery.level);
 	log("Battery charging: " + battery.charging);
 	log("Battery discharging time: ", battery.dischargingTime);
 	battery.addEventListener("chargingchange", function(e) {
 	  log("Battery chargingchange event: " + battery.charging);
 	}, false);
+}
+
+if(navigator.getBattery) {
+	navigator.getBattery().then(logBattery, function() {
+		log("There was an error while getting the battery state.");
+	});
+} else if (battery) {
+	logBattery(battery);
 } else {
 	log("Shame! The Battery API is not supported on this platform.")
 }
