8439412d54ece91a53c5718aae73790f55a661ef
Sam Dutton <dutton@google.com>
2015-01-06 17:10:48 +0000

    Added image examples

---
+++ pictureart/js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var img = document.querySelector('img');
+var imgSrc = document.querySelector('#imgSrc');
+var imgNaturalWidth = document.querySelector('#imgNaturalWidth');
+var imgWidth = document.querySelector('#imgWidth');
+
+var dpr = document.querySelector('#devicePixelRatio');
+var viewportWidth = document.querySelector('#viewportWidth');
+var availableWidth = document.querySelector('#availableWidth');
+
+function displayData(){
+  imgSrc.innerHTML = '<a href="' + img.currentSrc + '">' +
+    img.currentSrc.replace(/^.*[\\\/]/, '') + '</a>';
+  imgNaturalWidth.textContent = img.naturalWidth;
+  imgWidth.textContent = img.width;
+  dpr.textContent = window.devicePixelRatio;
+  viewportWidth.textContent = document.documentElement.clientWidth;
+  availableWidth.textContent = window.screen.availWidth;
+}
+
+img.onload = window.onresize = displayData;
+
+// in case image already loaded -- is there a better way?
+displayData();
---
+++ srcsetwvalues/js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var img = document.querySelector('img');
+var imgSrc = document.querySelector('#imgSrc');
+var imgNaturalWidth = document.querySelector('#imgNaturalWidth');
+var imgWidth = document.querySelector('#imgWidth');
+
+var dpr = document.querySelector('#devicePixelRatio');
+var minimumWidth = document.querySelector('#minimumWidth');
+var viewportWidth = document.querySelector('#viewportWidth');
+var availableWidth = document.querySelector('#availableWidth');
+
+function displayData(){
+  imgSrc.innerHTML = '<a href="' + img.currentSrc + '">' +
+    img.currentSrc.replace(/^.*[\\\/]/, '') + '</a>';
+  imgNaturalWidth.textContent = img.naturalWidth;
+  imgWidth.textContent = img.width;
+  dpr.textContent = window.devicePixelRatio;
+  minimumWidth.textContent =  img.width * window.devicePixelRatio;
+  viewportWidth.textContent = document.documentElement.clientWidth;
+  availableWidth.textContent = window.screen.availWidth;
+}
+
+img.onload = window.onresize = displayData;
+
+// in case image already loaded -- is there a better way?
+displayData();
