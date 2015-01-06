01b79d17910b416779ec3804130a166c41ae823a
Sam Dutton <dutton@google.com>
2015-01-06 17:09:38 +0000

    Tweaks to image examples

--- picturetype/js/main.js
+++ picturetype/js/main.js
@@ -1,2 +1,13 @@
-document.querySelector('#imgSrc').textContent =
-  document.querySelector('img').currentSrc.replace(/^.*[\\\/]/, '');
+'use strict';
+
+var img = document.querySelector('img');
+var imgSrc = document.querySelector('#imgSrc');
+
+img.onload = function(){
+  imgSrc.textContent =
+    img.currentSrc.replace(/^.*[\\\/]/, '');
+};
+
+// in case image already loaded -- is there a better way?
+imgSrc.textContent = img.currentSrc.replace(/^.*[\\\/]/, '');
+
