23e92b3414913255c28ccbd171d04dca0c6dc546
Sam Dutton <samdutton@gmail.com>
2015-12-15 11:54:16 +0000

    Fixed options setting in MediaRecorder constructor

--- mediarecorder/js/main.js
+++ mediarecorder/js/main.js
@@ -38,7 +38,7 @@ var isSecureOrigin = location.protocol === 'https:' ||
 location.host === 'localhost';
 if (!isSecureOrigin) {
   alert('getUserMedia() must be run from a secure origin: HTTPS or localhost.' +
-      '\n\nChanging protocol to HTTPS');
+    '\n\nChanging protocol to HTTPS');
   location.protocol = 'HTTPS';
 }
 
@@ -88,15 +88,22 @@ function toggleRecording() {
 }
 
 function startRecording() {
+  var options = {mimeType: 'video/vp9'};
   try {
     recordedBlobs = [];
-    mediaRecorder = new MediaRecorder(window.stream/* , 'video/vp8' */);
-  } catch (event) {
-    alert('MediaRecorder is not supported by this browser.\n\n' +
-      'Try Firefox 29 or later, or Chrome 47 or later, with Enable experimental Web Platform features enabled from chrome://flags.');
-    console.error('Exception while creating MediaRecorder:', event);
-    return;
+    mediaRecorder = new MediaRecorder(window.stream, options);
+  } catch (e) {
+    try {
+      options = 'video/vp8'; // Chrome 47
+      mediaRecorder = new MediaRecorder(window.stream, options);
+    } catch (event) {
+      alert('MediaRecorder is not supported by this browser.\n\n' +
+        'Try Firefox 29 or later, or Chrome 47 or later, with Enable experimental Web Platform features enabled from chrome://flags.');
+      console.error('Exception while creating MediaRecorder:', event);
+      return;
+    }
   }
+  console.log('Created MediaRecorder', mediaRecorder, ' with options', options);
   console.assert(mediaRecorder.state === 'inactive');
   recordButton.textContent = 'Stop Recording';
   playButton.disabled = true;
