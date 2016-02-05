c935669192a430ea41f53caf37f8401da07ce765
Sam Dutton <samdutton@gmail.com>
2016-02-05 21:42:45 +0000

    Slider resize working :)

--- video/split/js/main.js
+++ video/split/js/main.js
@@ -1,11 +1,23 @@
 'use strict';
 
-/* globals FileError */
 
-// get video file via XHR
-// store with File API
-// read Blob from File API and set as video src using createObjectUrl()
-// play video
+var container = document.getElementById('splitview');
+var video = container.querySelector('video');
+var sheet = document.styleSheets[0];
+var videoClientHeight;
 
-var video = document.querySelector('video');
+function setThumbHeight() {
+  if (videoClientHeight !== video.clientHeight) {
+    videoClientHeight = video.clientHeight;
+    var rule = 'input[type=range]::-webkit-slider-thumb {height: ' +
+      video.clientHeight + 'px !important}';
+    sheet.removeRule(1);
+    sheet.insertRule(rule, 1);
+    console.log('>>>>> sheet: ', sheet);
+    console.log('>>>>> video.clientHeight: ', video.clientHeight);
+  }
+}
+
+video.addEventListener('loadedmetadata', setThumbHeight);
+window.addEventListener('resize', setThumbHeight);
 
