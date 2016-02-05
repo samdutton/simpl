c5123c54c0b790248a05c8e09577f10f1c0e0042
Sam Dutton <samdutton@gmail.com>
2016-02-05 18:57:32 +0000

    Started work on creating split video view

---
+++ video/split/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* globals FileError */
+
+// get video file via XHR
+// store with File API
+// read Blob from File API and set as video src using createObjectUrl()
+// play video
+
+var video = document.querySelector('video');
+
