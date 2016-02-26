4b2c1756ef735466449e663df3bad27344ed56da
Sam Dutton <samdutton@gmail.com>
2016-02-26 18:40:58 +0000

    Added CSS variables and tweaked for Firefox

--- video/split/js/main.js
+++ video/split/js/main.js
@@ -3,19 +3,10 @@
 
 var container = document.getElementById('splitview');
 var video = container.querySelector('video');
-var sheet = document.styleSheets[0];
-var videoClientHeight;
 
 function setThumbHeight() {
-  if (videoClientHeight !== video.clientHeight) {
-    videoClientHeight = video.clientHeight;
-    var rule = 'input[type=range]::-webkit-slider-thumb {height: ' +
-      video.clientHeight + 'px !important}';
-    sheet.removeRule(1);
-    sheet.insertRule(rule, 1);
-    console.log('>>>>> sheet: ', sheet);
-    console.log('>>>>> video.clientHeight: ', video.clientHeight);
-  }
+  document.documentElement.style.setProperty('--video-height',
+    video.clientHeight + 'px');
 }
 
 video.addEventListener('loadedmetadata', setThumbHeight);
