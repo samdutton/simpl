13ee34b18525eb31737819cf4aaf22ac3424a2f7
Sam Dutton <samdutton@gmail.com>
2016-06-08 12:02:26 +0100

    Added range to playbackRate demo

--- video/playbackrate/js/main.js
+++ video/playbackrate/js/main.js
@@ -1,7 +1,20 @@
 var video = document.querySelector('video');
-video.playbackRate = 2;
+var range = document.querySelector('input[type=range]');
+var playbackRateDisplay = document.getElementById('playbackRate');
+
+playbackRateDisplay.textContent = video.defaultPlaybackRate;
+range.value = video.defaultPlaybackRate;
+
 video.addEventListener('loadedmetadata', function() {
   var fileName = this.currentSrc.replace(/^.*[\\\/]/, '');
   document.querySelector('#videoSrc').textContent =
     'Playing video: ' + fileName;
 });
+
+range.onchange = function(e) {
+  video.playbackRate = e.target.value;
+};
+
+video.onratechange = function() {
+  playbackRateDisplay.textContent = this.playbackRate;
+};
