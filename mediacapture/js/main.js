5f7e2bd70d513be8053d2fe4e07f9c55912029ae
Sam Dutton <samdutton@gmail.com>
2016-06-24 21:23:13 +0100

    Fixed video capture

--- mediacapture/js/main.js
+++ mediacapture/js/main.js
@@ -1,29 +1,37 @@
 'use strict';
 
 var img = document.querySelector('img');
-var input = document.querySelector('input');
+var video = document.querySelector('video');
+var inputs = document.querySelectorAll('input');
 
-input.onchange = function(inputEvent) {
+for (var i = 0; i !== inputs.length; ++i) {
+  inputs[i].onchange = handleInput;
+}
+
+function handleInput(inputEvent) {
   var files = inputEvent.target.files;
   if (files && files.length > 0) {
-    var file = files[0];
+    var file = window.file = files[0]; // global scope so visible in console
     console.log('file, files:', file, files);
+    var isVideo = file.type.indexOf('video') !== -1;
+    var displayElement = isVideo ? video : img;
+    displayElement.classList.remove('hidden');
     try {
-      var fileReader = new FileReader();
-      fileReader.onload = function(fileReaderEvent) {
-        img.src = fileReaderEvent.target.result;
-      };
-      fileReader.readAsDataURL(file);
-      console.log('fileReader', fileReader);
+      console.log('trying URL');
+      var url = window.URL.createObjectURL(file);
+      displayElement.src = url;
+      // window.URL.revokeObjectURL(url);
     } catch(event) {
       try {
-        var url = window.URL.createObjectURL(file);
-        img.src = url;
-        window.URL.revokeObjectURL(url);
+        var fileReader = new FileReader();
+        fileReader.onload = function(fileReaderEvent) {
+          displayElement.src = fileReaderEvent.target.result;
+        };
+        fileReader.readAsDataURL(file);
       } catch(error) {
         console.log('Neither createObjectURL or FileReader are supported',
           error);
       }
     }
   }
-};
+}
