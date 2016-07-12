aa09bf20cd1803e9c6500c414f0d6313d7121671
Sam Dutton <samdutton@gmail.com>
2016-07-12 19:42:32 +0100

    Added camera selection to imagecapture demo

--- imagecapture/js/main.js
+++ imagecapture/js/main.js
@@ -7,12 +7,14 @@
 
 // window.isSecureContext could be used for Chrome
 var isSecureOrigin = location.protocol === 'https:' ||
-    location.host === 'localhost';
+location.host === 'localhost';
 if (!isSecureOrigin) {
   alert('getUserMedia() must be run from a secure origin: HTTPS or localhost.' +
     '\n\nChanging protocol to HTTPS');
   location.protocol = 'HTTPS';
 }
+
+var constraints;
 var imageCapture;
 
 var grabFrameButton = document.querySelector('button#grabFrame');
@@ -21,37 +23,67 @@ var takePhotoButton = document.querySelector('button#takePhoto');
 var canvas = document.querySelector('canvas');
 var img = document.querySelector('img');
 var video = document.querySelector('video');
+var videoSelect = document.querySelector('select#videoSource');
 var zoomInput = document.querySelector('input#zoom');
-
-zoomInput.oninput = function() {
-  imageCapture.setOptions({
-    zoom: zoomInput.value
-  });
-};
 
 grabFrameButton.onclick = grabFrame;
 takePhotoButton.onclick = takePhoto;
+videoSelect.onchange = getVideo;
+zoomInput.oninput = setZoom;
 
-var constraints = {
-  audio: false,
-  video: true
-};
+navigator.mediaDevices.enumerateDevices().then(gotDevices).
+catch(function(error) {
+  console.log('Error getting devices: ', error);
+});
 
+getVideo();
 
-navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+function getVideo() {
+  // if (window.stream) {
+  //   window.stream.getTracks().forEach(function(track) {
+  //     track.stop();
+  //   });
+  // }
+  var videoSource = videoSelect.value;
+  constraints = {
+    audio: false,
+    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+  };
+  navigator.mediaDevices.getUserMedia(constraints).
+    then(gotStream).then(gotDevices).catch(function(error) {
+      console.log('getUserMedia error: ', error);
+    });
+}
+
+function gotDevices(deviceInfos) {
+  for (var i = 0; i !== deviceInfos.length; ++i) {
+    var deviceInfo = deviceInfos[i];
+    var option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || 'camera ' + (videoSelect.length + 1);
+      videoSelect.appendChild(option);
+    } else {
+      console.log('Some other kind of source/device: ', deviceInfo);
+    }
+  }
+}
+
+function gotStream(stream) {
   console.log('getUserMedia() got stream: ', stream);
   window.stream = stream; // global scope visible in browser console
   if (window.URL) {
     video.src = window.URL.createObjectURL(stream);
+    video.classList.remove('hidden');
   } else {
     video.src = stream;
   }
 
   imageCapture = window.imageCapture =
-    new ImageCapture(stream.getVideoTracks()[0]);
+  new ImageCapture(stream.getVideoTracks()[0]);
 
   setTimeout(getCapabilities, 100);
-});
+}
 
 function getCapabilities() {
   imageCapture.getPhotoCapabilities().then(function(capabilities) {
@@ -76,6 +108,12 @@ function grabFrame() {
     canvas.classList.remove('hidden');
   }).catch(function(error) {
     console.log('takePhoto() error: ', error);
+  });
+}
+
+function setZoom() {
+  imageCapture.setOptions({
+    zoom: zoomInput.value
   });
 }
 
