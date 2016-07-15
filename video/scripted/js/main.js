8582a97cfdd4c8b2ea662600bbd0a70b68ae2b88
Sam Dutton <samdutton@gmail.com>
2016-07-15 10:51:40 +0100

    Added promise to scripted playback example

--- video/scripted/js/main.js
+++ video/scripted/js/main.js
@@ -2,8 +2,20 @@
 
 var video = document.querySelector('video');
 
+var promise = video.play();
+
+// promise won’t be defined in browsers that don't support promisified play()
+if (promise === undefined) {
+  console.log('Promisified video play() not supported');
+} else {
+  promise.then(function() {
+    console.log('Video playback successfully initiated, returning a promise');
+  }).catch(function(error) {
+    console.log('Error initiating video playback: ', error);
+  });
+}
+
 video.onloadedmetadata = function() {
-  video.play();
   var fileName = this.currentSrc.replace(/^.*[\\\/]/, '');
   document.querySelector('#videoSrc').innerHTML = 'Playing video: ' + fileName;
 };
