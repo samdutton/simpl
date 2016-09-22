71db73e5ac61ba0905937e146a4838c8319aea05
Sam Dutton <samdutton@gmail.com>
2016-09-22 17:49:14 +0100

    Added HTTPS check

--- install/js/main.js
+++ install/js/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var isSecureOrigin = location.protocol === 'https:' ||
+  location.host === 'localhost';
+if (!isSecureOrigin) {
+  alert('getUserMedia() must be run from a secure origin: HTTPS or localhost.' +
+    '\n\nChanging protocol to HTTPS');
+  location.protocol = 'HTTPS';
+}
+
 var installButton = document.getElementById('install');
 
 if ('serviceWorker' in navigator) {
