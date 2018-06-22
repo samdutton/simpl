9458618cedd028fef2cf675ea1e938f3069dd110
Sam Dutton <samdutton@gmail.com>
2018-06-22 15:58:45 +0100

    Added link to blog post about teaching web dev

---
+++ getusermedia/webaudio/js/main.js
@@ -0,0 +1,57 @@
+/*
+Copyright 2017 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+'use strict';
+
+/* globals AudioContext, webkitAudioContext */
+
+// create an AudioContext
+// create an audio source node from the microphone via getUserMedia
+// connect it to a filter node
+// connect that to a gain node
+// play sound!
+
+// cope with browser differences
+let audioContext;
+if (typeof AudioContext === 'function') {
+  audioContext = new AudioContext();
+} else if (typeof webkitAudioContext === 'function') {
+  audioContext = new webkitAudioContext(); // eslint-disable-line new-cap
+} else {
+  console.log('Sorry! Web Audio is not supported by this browser');
+}
+
+// create a filter node
+var filterNode = audioContext.createBiquadFilter();
+// see https://dvcs.w3.org/hg/audio/raw-file/tip/webaudio/specification.html#BiquadFilterNode-section
+filterNode.type = 'highpass';
+// cutoff frequency: for highpass, audio is attenuated below this frequency
+filterNode.frequency.value = 10000;
+
+// create a gain node (to change audio volume)
+var gainNode = audioContext.createGain();
+// default is 1 (no change); less than 1 means audio is attenuated
+// and vice versa
+gainNode.gain.value = 0.5;
+
+navigator.mediaDevices.getUserMedia({audio: true}, (stream) => {
+  // Create an AudioNode from the stream
+  const mediaStreamSource = audioContext.createMediaStreamSource(stream);
+  mediaStreamSource.connect(filterNode);
+  filterNode.connect(gainNode);
+  // connect the gain node to the destination (i.e. play the sound)
+  gainNode.connect(audioContext.destination);
+});
