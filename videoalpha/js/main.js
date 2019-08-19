93d1d05efcb93ecbc242c2d2dae832a6d98d203f
Sam Dutton <samdutton@gmail.com>
2019-08-19 11:40:10 +0100

    More tweaks to videoalpha

--- videoalpha/js/main.js
+++ videoalpha/js/main.js
@@ -48,15 +48,15 @@ chromeVideo.onclick = toggleChromeVideo;
 containerDiv.onclick = toggleChromeVideo;
 
 function addEventListeners(video) {
-  video.ondblclick = handleVideoDblclick;
-  video.addEventListener('pointerdown', handlePointerDown, false);
-  video.addEventListener('pointerup', handlePointerUp, false);
-  video.addEventListener('pointermove', handlePointerMove, false);
+  video.ondblclick = handleDblclick;
+  video.onpointerdown = handlePointerDown;
+  video.onpointerup = handlePointerUp;
+  video.onpointermove = handlePointerMove;
 }
 addEventListeners(video1);
 addEventListeners(video2);
 
-function handleVideoDblclick(event) {
+function handleDblclick(event) {
   var video = event.srcElement;
   video.classList.remove('rotateOut');
   setTimeout(function() {
